60201ac4b38f0007ab839184bf52385398e355e8
IgorErin <erin.igore@yandex.ru>
2023-02-23 01:20:38 +0300

    add: comments

--- src/Introduction/Factorial.fs
+++ src/Introduction/Factorial.fs
@@ -1,5 +1,14 @@
 namespace Introduction
 
+/// <summary>
+/// Factorial module.
+/// </summary>
 module Factorial =
+    /// <summary>
+    /// Run factorial computation.
+    /// </summary>
+    /// <param name="count">
+    /// Number whose factorial will be calculated.
+    /// </param>
     let run count =
         if count > 0 then Seq.reduce (*) [ 1..count ] else 1
