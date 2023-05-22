c1fc38a3228af99f6985cfea5c2b89dd4c52be4a
IgorErin <erin.igore@yandex.ru>
2023-05-23 01:01:21 +0300

    add: tests

--- src/Introduction/Net.fs
+++ src/Introduction/Net.fs
@@ -1,97 +1,102 @@
 namespace Introduction.Net
 
-open Introduction
-open Expecto
 open FsCheck
 
-type OS =
-    | Windows
-    | Linux
-    | OsX
+type Probability =
+    | Always
+    | Often
+    | Rarely
+    | Never
 
 type Id = string
 
-type Predicate(probability: float) =
-    do
-        if probability < 0 || probability > 1 then
-            failwith "Probability must be in range"
-
-    let floatIsEqual x y =
-        abs (x - y) < Accuracy.medium.absolute || x.Equals y
-
+type Predicate(probability: Probability) =
     let trueGen = Gen.constant true
     let falseGen = Gen.constant false
 
-    let count = int <| probability * 100.0
+    let maxProbability = 100
 
-    let frequency = [ (count, trueGen); (100 - count, falseGen) ]
+    let frequency count =
+        [ (count, trueGen); (maxProbability - count, falseGen) ]
+
+    // map Gen frequency to bool
+    let frequencyToResult frequency =
+        frequency |> Gen.frequency |> Gen.sample 1 1 |> List.last
 
     member this.Run() =
-        Gen.frequency frequency |> Gen.sample 1 1 |> List.last
+        match probability with
+        | Always -> true
+        | Often ->
+            // hardcoded frequency
+            frequency 75 |> frequencyToResult
+        | Rarely ->
+            // agan
+            frequency 25 |> frequencyToResult
+        | Never -> false
 
-    member this.IsZeroProbability = floatIsEqual probability 0.0
+    member this.IsZeroProbability =
+        match probability with
+        | Never -> true
+        | _ -> false
 
-type INode =
-    abstract IsIll: bool
+type IInfectNode =
+    abstract IsInfected: bool
 
     abstract Id: Id
 
     abstract TryInfect: unit -> unit
 
-    abstract CanIll: bool
+    abstract CanGetInfected: bool
 
-type Node(isIll, predicate: Predicate, id: Id) =
-    let mutable isIll = isIll
+type InfectNode(isInfected, predicate: Predicate, id: Id) =
+    let mutable isInfected = isInfected
 
-    interface INode with
-        member this.IsIll = isIll
+    interface IInfectNode with
+        member this.IsInfected = isInfected
 
         member val Id = id with get
 
-        member val CanIll = predicate.IsZeroProbability
+        member val CanGetInfected = not predicate.IsZeroProbability
 
         member this.TryInfect() =
-            isIll <- predicate.Run()
+            if not isInfected then
+                isInfected <- predicate.Run()
 
 type IInfectNet =
-    abstract RunStep: unit -> unit
+    abstract Infect: unit -> unit
 
-    abstract GetState: unit -> (Id * bool) array
+    abstract InfectionIsOver: unit -> bool
 
-type InfectNet(adjacencyMatrix: bool[,], nodes: INode[]) =
-    do
-        // validate
-        if Array2D.length1 adjacencyMatrix <> Array2D.length2 adjacencyMatrix then
-            failwith "The matrix must be square"
+    abstract State: unit -> (Id * bool) list
 
-        if Array2D.length1 adjacencyMatrix <> Array.length nodes then
-            failwith "The length of the matrix must be equal to the number of nodes"
-
-        nodes
-        |> Array.distinctBy (fun node -> node.Id)
-        |> Array.length
-        |> fun length ->
-            if length <> Array.length nodes then
-                failwith "Node IDs should be different"
-
-    /// create lookup table consisting of (node, his neighbors)
-    let table =
-        (adjacencyMatrix, nodes)
-        ||> Array2D.mapByRow (fun isConnected node ->
-            if isConnected then Some node else None
-        )
-        |> Array.map (Array.choose id)
-        |> Array.map2 (fun node neighbors -> node, neighbors) nodes
+type InfectNet(table: (IInfectNode * IInfectNode list) list) =
+    let extractInfectedNodeNeighbors () =
+        table
+        // extract neighbors of ill nodes
+        |> List.filter (fst >> fun node -> node.IsInfected)
+        |> List.map snd
+        // concat them
+        |> List.concat
+        |> List.distinctBy (fun node -> node.Id)
 
     interface IInfectNet with
-        member this.RunStep() =
-            table
-            // extract neighbors of ill nodes
-            |> Array.filter (fst >> fun node -> node.IsIll)
-            |> Array.map snd
-            |> Array.concat
-            // try infect them
-            |> Array.iter (fun node -> node.TryInfect())
+        member this.InfectionIsOver() =
+            extractInfectedNodeNeighbors ()
+            // among them we are looking for those who may be infected
+            |> List.exists (fun node -> node.CanGetInfected && not <| node.IsInfected)
+            |> not
 
-        member this.GetState() =
-            nodes |> Array.map (fun node -> node.Id, node.IsIll)
+        member this.Infect() =
+            extractInfectedNodeNeighbors ()
+            // try infect them
+            |> List.iter (fun node -> node.TryInfect())
+
+        member this.State() =
+            let firstItems = List.map fst table
+            let secondItems = List.map snd table |> List.concat
+
+            let allNodes =
+                List.concat [ firstItems; secondItems ]
+                |> List.distinctBy (fun node -> node.Id)
+
+            allNodes |> List.map (fun node -> node.Id, node.IsInfected)
--- tests/FSharpHomeworks.Tests/InfectNet.fs
+++ tests/FSharpHomeworks.Tests/InfectNet.fs
@@ -3,17 +3,128 @@ module FSharpHomeworks.Tests.InfectNet
 open Expecto
 open Introduction.Net
 
-let createTest name matrix nodes expectedResult =
-    testCase
-        name
-        (fun () ->
-            let net = InfectNet(matrix, nodes)
+let createTest
+    name
+    (net: IInfectNet)
+    (expectedState: (Id * bool) list)
+    (expectedIterationCount: int option)
+    =
+    test name {
+        let maxIterationCount = 1000
 
-            (net :> IInfectNet).RunStep()
+        let mutable iterationCount = 0
 
-            let actualResult = (net :> IInfectNet).GetState()
+        while not <| net.InfectionIsOver() && iterationCount < maxIterationCount do
 
-            "Results must be the same" |> Expect.equal actualResult expectedResult
-        )
+            net.Infect()
+            iterationCount <- iterationCount + 1
 
-let sourceMatrix: bool[,] = Array2D.init 3 3 (fun id1 id2 -> id1 = id2)
+        match expectedIterationCount with
+        | Some value ->
+            "Iteration count must be the same" |> Expect.equal iterationCount value
+        | None -> ()
+
+        List.iter
+            (fun (id, isInfect) ->
+                let actual = List.find ((>>) fst <| (=) id) <| net.State() |> snd
+
+                $"Result must be the same. Node id: %A{id}"
+                |> Expect.equal actual isInfect
+            )
+            expectedState
+    }
+
+let createLinkedNet predicate =
+    let predicate = Predicate(predicate)
+
+    let firstNode: IInfectNode = InfectNode(false, predicate, "first")
+    let secondNode: IInfectNode = InfectNode(false, predicate, "second")
+    let thirdNode: IInfectNode = InfectNode(false, predicate, "third")
+
+    let forthNode: IInfectNode = InfectNode(true, predicate, "forth")
+
+    let net =
+        [ forthNode, [ thirdNode ]
+          thirdNode, [ secondNode ]
+          secondNode, [ firstNode ] ]
+
+    InfectNet(net)
+
+let linkedTests =
+    [
+      // always
+      let alwaysNet = createLinkedNet Always
+      let expectedResult = [ "first", true; "second", true; "third", true; "forth", true ]
+      let expectedIterationCount = 3
+
+      createTest "always" alwaysNet expectedResult <| Some expectedIterationCount
+
+      // often
+      let oftenNet = createLinkedNet Often
+
+      createTest "often" oftenNet expectedResult None
+
+      // rare
+      let rareNet = createLinkedNet Rarely
+
+      createTest "rare" rareNet expectedResult None
+
+      // never
+      let expectedResult =
+          [ "first", false; "second", false; "third", false; "forth", true ]
+
+      let neverNet = createLinkedNet Never
+
+      createTest "never" neverNet expectedResult None ]
+    |> testList "linked"
+
+let createBipartiteNet predicate =
+    let predicate = Predicate(predicate)
+
+    let firstNode: IInfectNode = InfectNode(false, predicate, "first")
+    let secondNode: IInfectNode = InfectNode(false, predicate, "second")
+    let thirdNode: IInfectNode = InfectNode(false, predicate, "third")
+
+    let forthNode: IInfectNode = InfectNode(true, predicate, "forth")
+
+    let net =
+        [ forthNode, [ thirdNode ]
+          thirdNode, [ forthNode ]
+
+          secondNode, [ firstNode ]
+          firstNode, [ secondNode ] ]
+
+    InfectNet(net)
+
+let bipartiteTests =
+    [ let alwaysNet = createBipartiteNet Always
+
+      let expectedResult =
+          [ "first", false
+            "second", false
+
+            "third", true
+            "forth", true ]
+
+      createTest "always" alwaysNet expectedResult None
+
+      // often
+      let oftenNet = createBipartiteNet Often
+
+      createTest "often" oftenNet expectedResult None
+
+      // rare
+      let rareNet = createBipartiteNet Rarely
+
+      createTest "rare" rareNet expectedResult None
+
+      // never
+      let expectedResult =
+          [ "first", false; "second", false; "third", false; "forth", true ]
+
+      let neverNet = createLinkedNet Never
+
+      createTest "never" neverNet expectedResult None ]
+    |> testList "bipartite"
+
+let allTests = testList "Infect net" [ linkedTests; bipartiteTests ]
