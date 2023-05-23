025a6b84df090de2e978b4607685ddb29f57e2f8
IgorErin <erin.igore@yandex.ru>
2023-05-23 17:33:47 +0300

    wip

--- src/Introduction/Program.fs
+++ src/Introduction/Program.fs
@@ -1,2 +1,42 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Crawler
+
+open System.Net.Http
+open System.Text.RegularExpressions
+
+module Static =
+    let client = new HttpClient()
+    
+    let regex = Regex(@"a href=""http:(\S*)")
+       
+type Either<'a> = Left of string | Right of 'a
+    
+let getPageAsync (url: string) = async {
+    try 
+        let! response =
+            Static.client.GetAsync(url) |> Async.AwaitTask
+
+        let! response =
+            response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        
+        return Right response
+    with e ->
+        return Left e.Message
+}
+
+let runAsync url = async {
+    let! page = getPageAsync url
+    
+    match page with
+    | Right page ->
+        Static.regex.Matches(page)
+        |> Seq.map (fun url -> url.[8
+        
+        ()
+    
+    ()
+}
+    
+
+let getCount url = async {
+    
+}
